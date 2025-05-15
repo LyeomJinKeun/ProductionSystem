7ad23b6f615780335d98a7620a446f0b0cde5c9b
LyeomJinKeun <angel426612@naver.com>
2025-05-15 17:14:16 +0900

    임시 페이지 생성

---
+++ ProductionSystem.Web/Controllers/AppsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductionSystem.Web.Controllers
+{
+    public class AppsController : Controller
+    {
+        public IActionResult System() => View("UnderConstruction");
+        public IActionResult Recipe() => View("UnderConstruction");
+        public IActionResult Monitor() => View("UnderConstruction");
+        public IActionResult Inspection() => View("UnderConstruction");
+    }
+}
